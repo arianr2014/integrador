fe73a52dc544e7b6663a1150b9df759f8615528a
Gianfranco Flores <gianfrancoflores.pacheco@gmail.com>
2024-07-13 23:32:54 -0500

    IMPLEMENTACIÓN BOTON EXPORTAR PDF

---
+++ src/main/java/Entidades/imprimirPedido.java
@@ -0,0 +1,61 @@
+package Entidades;
+
+public class imprimirPedido {
+    private String idPedido;
+    private String descripcion;
+    private double precio;
+    private double cantidad;
+    private double igv;
+    private double total;
+
+    public imprimirPedido() {
+    }
+
+    public String getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(String idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getIgv() {
+        return igv;
+    }
+
+    public void setIgv(double igv) {
+        this.igv = igv;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+}
